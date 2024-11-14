285e50e6862cb2a0e8969c97c7741abb34aa5a5b
Nicolò Ferronato <nicolo.ferronato@pearson.com>
2024-11-14 18:07:29 +0100

    changes added

---
+++ src/BiddingService/Program.cs
@@ -0,0 +1,40 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddMassTransit(x =>
+{
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context); 
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.Authority = builder.Configuration["IdentityServiceUrl"];
+        o.RequireHttpsMetadata = false;
+        o.TokenValidationParameters.ValidateAudience = false;
+        o.TokenValidationParameters.NameClaimType = "username";
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+app.MapControllers();
+await DB.InitAsync("BidDb",
+    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("BidDbConnection")));
+app.Run();
+
